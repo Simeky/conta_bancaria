3e01fe939e1a6e6ac48374ffa5f89bc17b181298
Simeky <guilherme.rayser@gmail.com>
2025-05-13 19:40:35 -0300

    java compiler in maven 1.7 -> 1.8

--- src/main/java/br/unisenai/App.java
+++ src/main/java/br/unisenai/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        if (validar_cpf("10212009990")) {
+          
+            System.out.println("addawd");
+            
+        }   else {
+
+                System.out.println("não");
+
+            }
+        
     }
 }
--- src/main/java/utilitarios/utils.java
+++ src/main/java/utilitarios/utils.java
@@ -24,7 +24,45 @@ public class utils {
 
         String cpf_n_formatado = so_numeros(cpf);
 
-        return true;
+        int sm = 0, r, num, peso = 10; 
+        
+        for (byte i= 0; i < 9; i++) {
+            num = (cpf_n_formatado.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+        }
+            
+        r = 11 - (sm % 11);
+            
+        char dig10;
+        if ((r == 10) || (r == 11)) {
+            dig10 = '0';
+        }   else {
+            	dig10 = (char)(r + 48);
+            }
+            
+        sm = 0;
+        peso = 11;
+            
+        for(i=0; i<10; i++) {
+            num = (cpf.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+        }
+            
+        r = 11 - (sm % 11);
+            
+        if ((r == 10) || (r == 11)) {
+        	dig11 = '0';
+        }   else {
+        	    dig11 = (char)(r + 48);
+            }
+
+        if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10))) {
+            	return(true);
+        }   else {
+            	return(false);
+            }
         
     }
 
