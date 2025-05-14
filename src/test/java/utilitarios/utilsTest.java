4fda1cc835d3d26b92850635265be7fa4780a4dc
Simeky <guilherme.rayser@gmail.com>
2025-05-13 21:15:07 -0300

    C: test for validar_cpf function

--- src/test/java/utilitarios/utilsTest.java
+++ src/test/java/utilitarios/utilsTest.java
@@ -5,21 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for utils functions.
  */
 public class utilsTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void test()
-    {
+    public void test_so_numeros() {
 
         String cpf = "364.733.009-45";
+        String cpf_erro = "1a2cadQRE3a.,65=4";
 
         assertEquals("36473300945", utils.so_numeros(cpf));
+        assertNotEquals("12a3654", utils.so_numeros(cpf_erro));
+
+    }
+
+    @Test
+    public void test_validar_cpf() {
+
+        String cpf = "364.733.009-45";
+        String cpf_erro = "1a2cadQRE3a.,65=4";
+
         assertEquals(true, utils.validar_cpf(cpf));
+        assertNotEquals(true, utils.validar_cpf(cpf_erro));
 
     }
 }
