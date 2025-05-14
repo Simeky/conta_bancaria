fdc30fa6614a4327a41fd05aed29b42a8722904d
Simeky <guilherme.rayser@gmail.com>
2025-05-14 19:11:36 -0300

    C: EnderecamentoTest

---
+++ src/test/java/br/unisenai/classes/EnderecamentoTest.java
@@ -0,0 +1,22 @@
+package br.unisenai.classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EnderecamentoTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test()
+    {
+
+        assertTrue(true);
+
+    }
+}
