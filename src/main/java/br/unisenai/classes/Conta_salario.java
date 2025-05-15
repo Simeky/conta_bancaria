9667b27aae067356ca2113c90f7c17e4a7dd1d12
Simeky <guilherme.rayser@gmail.com>
2025-05-15 19:24:41 -0300

    C: variables, getters, setters

--- src/main/java/br/unisenai/classes/Conta_salario.java
+++ src/main/java/br/unisenai/classes/Conta_salario.java
@@ -1,5 +1,35 @@
 package br.unisenai.classes;
 
-public class Conta_salario {
+import java.sql.Date;
+
+public class Conta_salario extends Conta_bancaria{
+
+    private Pessoa_juridica cs_empresa;
+    private Date cs_limite_adiantamento;
+    private Conta_bancaria cs_conta;
+
+    public Pessoa_juridica getCs_empresa() {
+        return cs_empresa;
+    }
+
+    public void setCs_empresa(Pessoa_juridica cs_empresa) {
+        this.cs_empresa = cs_empresa;
+    }
+
+    public Date getCs_limite_adiantamento() {
+        return cs_limite_adiantamento;
+    }
+
+    public void setCs_limite_adiantamento(Date cs_limite_adiantamento) {
+        this.cs_limite_adiantamento = cs_limite_adiantamento;
+    }
+
+    public Conta_bancaria getCs_conta() {
+        return cs_conta;
+    }
+    
+    public void setCs_conta(Conta_bancaria cs_conta) {
+        this.cs_conta = cs_conta;
+    }
 
 }
