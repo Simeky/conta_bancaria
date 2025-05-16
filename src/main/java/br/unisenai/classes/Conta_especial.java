b15c4042183e8ae99a1bc15bb23d3c733b4fee1f
Simeky <guilherme.rayser@gmail.com>
2025-05-15 21:08:47 -0300

    C: variables, constructor, getters, setters -> Conta_especial

--- src/main/java/br/unisenai/classes/Conta_especial.java
+++ src/main/java/br/unisenai/classes/Conta_especial.java
@@ -1,5 +1,32 @@
 package br.unisenai.classes;
 
-public class Conta_especial {
+import java.sql.Date;
+
+public class Conta_especial extends Conta_bancaria{
+
+    private double ce_limite_cred;
+    private Date ce_vencimento_cred;
+
+    public Conta_especial() {
+
+        super();
+
+    }
+
+    public double getCe_limite_cred() {
+        return ce_limite_cred;
+    }
+
+    public void setCe_limite_cred(double ce_limite_cred) {
+        this.ce_limite_cred = ce_limite_cred;
+    }
+
+    public Date getCe_vencimento_cred() {
+        return ce_vencimento_cred;
+    }
+
+    public void setCe_vencimento_cred(Date ce_vencimento_cred) {
+        this.ce_vencimento_cred = ce_vencimento_cred;
+    }    
 
 }
