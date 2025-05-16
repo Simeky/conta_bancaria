3e681d32e3938910c424e4b621af2825d0b58fd2
Simeky <guilherme.rayser@gmail.com>
2025-05-15 21:16:33 -0300

    C: variables, constructor, getters, setters -> Movimentacao | U: Conciliado, Estornado -> eSituacao

--- src/main/java/br/unisenai/enums/eSituacao.java
+++ src/main/java/br/unisenai/enums/eSituacao.java
@@ -3,6 +3,8 @@ package br.unisenai.enums;
 public enum eSituacao {
 
     Ativa       (15),
+    Conciliado  (13),
+    Estornado   (12),
     Bloqueada   (10),
     Inativa     (5),
     Encerrada   (0); 
