b28f98377e52bfd3f6451b932a4783fa8240b556
Simeky <guilherme.rayser@gmail.com>
2025-05-20 19:58:44 -0300

    C: functions conectar, desconectar

--- src/main/java/connections/MySQL.java
+++ src/main/java/connections/MySQL.java
@@ -1,7 +1,40 @@
 package connections;
 
-public class MySQL {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MySQL {
     
+    public static Connection conectar() {        
+        
+        final String url         = "jdbc:mysql//localhost/bd_controle_bancario";
+        final String user        = "root";
+        final String password    = "";
+
+        try {
+
+            return DriverManager.getConnection(url, user, password);
+            
+        }   catch (SQLException e) {
+                throw new RuntimeException("A conexão falhou:\n" + e.getMessage());
+            }
+
+    }
+
+    public static void desconectar(Connection conexao) {
+
+        if (conexao != null) {
+
+            try {
+                conexao.close();
+
+            }   catch (SQLException e) {
+                    throw new RuntimeException("Problemas ao desconectar:\n" + e.getMessage());
+                }
+
+        }
+
+    }
 
 }
