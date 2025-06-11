9e3c2ce5578cf36da17699f1207c633b0d262f88
Simeky <guilherme.rayser@gmail.com>
2025-06-11 18:59:14 -0300

    Criada classe de teste para Pessoa_fisica_controller

---
+++ src/test/java/controllers/Pessoa_fisica_controllerTest.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import org.junit.Test;
+
+public class Pessoa_fisica_controllerTest {
+    @Test
+    public void testDesativar() {
+
+    }
+
+    @Test
+    public void testFind_all() {
+
+    }
+
+    @Test
+    public void testFind_pessoa_fisica() {
+
+    }
+
+    @Test
+    public void testFind_pessoa_fisica2() {
+
+    }
+
+    @Test
+    public void testInsert() {
+
+    }
+
+    @Test
+    public void testSave() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+}
