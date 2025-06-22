4ad5442164572ec3e0821ccb96cc2430ad03a442
Simeky <guilherme.rayser@gmail.com>
2025-06-22 20:10:58 -0300

    Criado teste unitário para o controller Conta_poupanca_controller

---
+++ src/test/java/controllers/Conta_poupanca_controllerTest.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import org.junit.Test;
+
+public class Conta_poupanca_controllerTest {
+    @Test
+    public void testCancela() {
+
+    }
+
+    @Test
+    public void testCancela2() {
+
+    }
+
+    @Test
+    public void testDesativa() {
+
+    }
+
+    @Test
+    public void testDesativa2() {
+
+    }
+
+    @Test
+    public void testFind_all() {
+
+    }
+
+    @Test
+    public void testFind_conta_poupanca() {
+
+    }
+
+    @Test
+    public void testInsert() {
+
+    }
+
+    @Test
+    public void testSave() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
+}
